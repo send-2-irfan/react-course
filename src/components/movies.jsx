a3e0238932febbc97e5eeb64fa05f9247a33f5b9
send-2-irfan <irfanullah.bses19@iba-suk.edu.pk>
2023-03-02 07:28:58 +0500

    done with rendering API data and with delete

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -4,28 +4,35 @@ import { getMovies } from '../services/fakeMovieService';
 class Movies extends Component {
 
     state = { movies:  getMovies()} 
+
+    handleDelete = (movie) =>{
+        const movies = this.state.movies.filter(m => m._id !== movie._id);
+        this.setState({movies})
+    }
     render() { 
         return (
             <React.Fragment>
                 <h2 className='h2 m-2'>Showing {this.state.movies.length} movies in the database.</h2>
-                <table class="table">
+                <table className="table">
                     <thead>
                     <tr>
                     <th scope="col">Title</th>
                     <th scope="col">Genre</th>
                     <th scope="col">Stock</th>
                     <th scope="col">Rate</th>
+                    <th></th>
                     </tr>
                 </thead>
                 <tbody>  
                                   
                     {
                         this.state.movies.map(movie =>
-                            <tr>
+                            <tr key={movie._id}>
                                 <td>{movie.title}</td>
                                 <td>{movie.genre.name}</td>
                                 <td>{movie.numberInStock}</td>
-                                <td>{movie.dailyRentalRate}</td>                           
+                                <td>{movie.dailyRentalRate}</td> 
+                                <td><button onClick={() => this.handleDelete(movie)} className="btn btn-danger btn-sm">Delete</button></td>                        
                                 </tr>
                         )
                     }
