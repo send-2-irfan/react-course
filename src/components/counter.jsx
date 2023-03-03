7acc5f36f43f128e5cb713f4d2a3d078c90f3be9
send-2-irfan <irfanullah.bses19@iba-suk.edu.pk>
2023-03-04 00:46:05 +0500

    updated

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,7 +3,7 @@
  class Counter extends Component {
    
     state ={
-        value: this.props.value,
+        value:this.props.value,
     }
 
     handleIncrement  = ()=>{
@@ -14,6 +14,7 @@
     render() { 
         return (
             <div>
+                <h4>{this.props.id}</h4>
                 <span className={ this.getBadgeClasses() } style={{ fontSize:30 , fontWeight: 'bold'}}>{this.formatCount()}</span>     
                 <button className='btn btn-secondary btn-sm' onClick={() => this.handleIncrement(this.state.value)}>Increment</button>
                 {/* <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul> */}
